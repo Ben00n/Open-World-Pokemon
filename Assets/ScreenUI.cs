544847b51a8f01ab186cdbe4a912f4a30bc507eb
Benoon <benbahar321@gmail.com>
2022-01-04 17:27:54 +0200

    Added ScreenUI for party

---
+++ Assets/ScreenUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenUI : MonoBehaviour
+{
+    PokemonPartyManager pokemonPartyManager;
+    PartyMemberUI[] memberSlots;
+
+    private void Awake()
+    {
+        memberSlots = GetComponentsInChildren<PartyMemberUI>(true);
+        pokemonPartyManager = FindObjectOfType<PokemonPartyManager>();
+    }
+
+    public void SetPartyData(List<GameObject> pokemons)
+    {
+        for (int i = 0; i < memberSlots.Length; i++)
+        {
+            if (i < pokemons.Count)
+            {
+                memberSlots[i].gameObject.SetActive(true);
+                memberSlots[i].SetData(pokemons[i].GetComponent<PokemonStatsCalculator>());
+            }
+            else
+                memberSlots[i].gameObject.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        SetPartyData(pokemonPartyManager.pokemons);
+    }
+}
--- Assets/Scripts/PartyMemberUI.cs
+++ Assets/Scripts/PartyMemberUI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
