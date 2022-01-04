59ff0bc51e6dd4f5bd90baf2a1b214cf98a2c8d9
Benoon <benbahar321@gmail.com>
2022-01-04 22:01:17 +0200

    Sorting of sprites & Implemention of PartyUI

---
+++ Assets/Scripts/Catch Related Scripts/ConsumableItem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsumableItem : Item
+{
+    [Header("Item Quantity")]
+    public int maxItemAmount;
+    public int currentItemAmount;
+
+    [Header("Item Model")]
+    public GameObject itemModel;
+
+    [Header("Animations")]
+    public string consumeAnimation;
+
+
+    public virtual void AttemptToConsumeItem(AnimatorManager animatorManager,RightHandHolderSlot rightHandHolderSlot,CameraManager cameraManager)
+    {
+        if (currentItemAmount > 0)
+        {
+            animatorManager.PlayTargetAnimation("Throw", true);
+        }
+        else
+        {
+            Debug.Log("failed no quantity");
+        }
+    }
+
+    public virtual void SuccessfullyConsumeItem(AnimatorManager animatorManager, RightHandHolderSlot rightHandHolderSlot, CameraManager cameraManager)
+    {
+        Debug.Log("you successfully consumed item");
+    }
+}
